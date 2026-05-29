098d5e81f61ff08e5abf5d2370d6bd5ae3c37131
Claudio Matheus <claudio.silva@smartbreeder.com.br>
2026-05-29 13:38:13 -0300

    refactor: remove obsolete logging components and related DTOs

--- src/ObservaNet.Shared/Dtos/BulkLogRequest.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace ObservaNet.Shared.Dtos
-{
-    public class BulkLogRequest
-    {
-        public List<LogEventDto> Events { get; set; } = new List<LogEventDto>();
-    }
-}
